08430585ee1ec6292577c840325645bd869f9b4f
Muhammad Tahir <m.tahiridrees27@gmail.com>
2023-12-11 17:46:18 +0500

    Updating Tables

--- pages/manager/index.js
+++ pages/manager/index.js
@@ -1,120 +1,21 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import { Box, Typography } from "@mui/material";
 import Card from "@mui/material/Card";
 import IconButton from "@mui/material/IconButton";
-import PropTypes from "prop-types";
-import { useTheme } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableHead from "@mui/material/TableHead";
-import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableFooter from "@mui/material/TableFooter";
-import TablePagination from "@mui/material/TablePagination";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-import FirstPageIcon from "@mui/icons-material/FirstPage";
-import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
-import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
-import LastPageIcon from "@mui/icons-material/LastPage";
 import Tooltip from "@mui/material/Tooltip";
 import DeleteIcon from "@mui/icons-material/Delete";
 import DriveFileRenameOutlineIcon from "@mui/icons-material/DriveFileRenameOutline";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
-import { styled } from "@mui/material/styles";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import CloseIcon from "@mui/icons-material/Close";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 import { getManagerFunApi } from "store/manager/services";
-
-// Add Task Modal
-const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-  "& .MuiDialogContent-root": {
-    padding: theme.spacing(2),
-  },
-  "& .MuiDialogActions-root": {
-    padding: theme.spacing(1),
-  },
-}));
-
-function ToDoList(props) {
-  const theme = useTheme();
-  const { count, page, rowsPerPage, onPageChange } = props;
-
-  const handleFirstPageButtonClick = (event) => {
-    onPageChange(event, 0);
-  };
-
-  const handleBackButtonClick = (event) => {
-    onPageChange(event, page - 1);
-  };
-
-  const handleNextButtonClick = (event) => {
-    onPageChange(event, page + 1);
-  };
-
-  const handleLastPageButtonClick = (event) => {
-    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
-  };
-
-  return (
-    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
-      <IconButton
-        onClick={handleFirstPageButtonClick}
-        disabled={page === 0}
-        aria-label="first page"
-      >
-        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
-      </IconButton>
-      <IconButton
-        onClick={handleBackButtonClick}
-        disabled={page === 0}
-        aria-label="previous page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowRight />
-        ) : (
-          <KeyboardArrowLeft />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleNextButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="next page"
-      >
-        {theme.direction === "rtl" ? (
-          <KeyboardArrowLeft />
-        ) : (
-          <KeyboardArrowRight />
-        )}
-      </IconButton>
-      <IconButton
-        onClick={handleLastPageButtonClick}
-        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
-        aria-label="last page"
-      >
-        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
-      </IconButton>
-    </Box>
-  );
-}
-
-ToDoList.propTypes = {
-  count: PropTypes.number.isRequired,
-  onPageChange: PropTypes.func.isRequired,
-  page: PropTypes.number.isRequired,
-  rowsPerPage: PropTypes.number.isRequired,
-};
+import { CustomPaginationTable } from "@/components/Table/CustomPaginationTable";
+import Link from "next/link";
 
 const Manager = () => {
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(8);
-  const router = useRouter();
   const dispatch = useDispatch();
-  const [rows, setRows] = useState([]);
   const { managers } = useSelector((state) => state.manager);
   const { business } = useSelector((state) => state.business);
 
@@ -127,25 +28,6 @@ const Manager = () => {
       );
     }
   }, [business?.id, dispatch, managers.managerFetch]);
-  useEffect(() => {
-    setRows(managers);
-  }, [managers]);
-
-  // Avoid a layout jump when reaching the last page with empty rows.
-  const emptyRows =
-    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
-
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    setRowsPerPage(parseInt(event.target.value, 10));
-    setPage(0);
-  };
-  const handleNavigate = () => {
-    router.push("/manager/add-manager");
-  };
 
   return (
     <>
@@ -177,224 +59,162 @@ const Manager = () => {
           >
             My Managers
           </Typography>
-
-          <Button
-            onClick={handleNavigate}
-            variant="contained"
-            sx={{
-              textTransform: "capitalize",
-              borderRadius: "8px",
-              fontWeight: "500",
-              fontSize: "13px",
-              padding: "12px 20px",
-              color: "#fff !important",
-            }}
-          >
-            <AddIcon
-              sx={{ position: "relative", top: "-1px" }}
-              className="mr-5px"
-            />{" "}
-            Add Manager
-          </Button>
+          <Link href="/manager/add-manager">
+            <Button
+              variant="contained"
+              sx={{
+                textTransform: "capitalize",
+                borderRadius: "8px",
+                fontWeight: "500",
+                fontSize: "13px",
+                padding: "12px 20px",
+                color: "#fff !important",
+              }}
+            >
+              <AddIcon
+                sx={{ position: "relative", top: "-1px" }}
+                className="mr-5px"
+              />{" "}
+              Add Manager
+            </Button>
+          </Link>
         </Box>
 
-        <TableContainer
-          component={Paper}
-          sx={{
-            boxShadow: "none",
-          }}
-        >
-          <Table
-            sx={{ minWidth: 930 }}
-            aria-label="custom pagination table"
-            className="dark-table"
-          >
-            <TableHead sx={{ background: "#F7FAFF" }}>
-              <TableRow>
-                <TableCell
+        <CustomPaginationTable
+          isLoading={managers.isLoading}
+          tableData={managers}
+          tableHeaderData={
+            <>
+              <TableCell
+                sx={{
+                  borderBottom: "1px solid #F7FAFF",
+                  fontSize: "13.5px",
+                }}
+              >
+                Sr.
+              </TableCell>
+              <TableCell
+                sx={{
+                  borderBottom: "1px solid #F7FAFF",
+                  fontSize: "13.5px",
+                }}
+              >
+                Name
+              </TableCell>
+
+              <TableCell
+                sx={{
+                  borderBottom: "1px solid #F7FAFF",
+                  fontSize: "13.5px",
+                }}
+              >
+                Email
+              </TableCell>
+
+              <TableCell
+                sx={{
+                  borderBottom: "1px solid #F7FAFF",
+                  fontSize: "13.5px",
+                }}
+              >
+                Phone Number
+              </TableCell>
+
+              <TableCell
+                align="right"
+                sx={{
+                  borderBottom: "1px solid #F7FAFF",
+                  fontSize: "13.5px",
+                }}
+              >
+                Action
+              </TableCell>
+            </>
+          }
+          tableBodyData={(data, index) => (
+            <>
+              <TableCell
+                sx={{
+                  fontWeight: "500",
+                  fontSize: "13px",
+                  borderBottom: "1px solid #F7FAFF",
+                  color: "#260944",
+                  pt: "16px",
+                  pb: "16px",
+                }}
+              >
+                {index}
+              </TableCell>
+              <TableCell
+                sx={{
+                  fontWeight: "500",
+                  fontSize: "13px",
+                  borderBottom: "1px solid #F7FAFF",
+                  color: "#260944",
+                  pt: "16px",
+                  pb: "16px",
+                }}
+              >
+                {data.name}
+              </TableCell>
+
+              <TableCell
+                sx={{
+                  borderBottom: "1px solid #F7FAFF",
+                  fontSize: "13px",
+                  pt: "16px",
+                  pb: "16px",
+                }}
+              >
+                {data.email}
+              </TableCell>
+
+              <TableCell
+                sx={{
+                  borderBottom: "1px solid #F7FAFF",
+                  fontSize: "13px",
+                  pt: "16px",
+                  pb: "16px",
+                }}
+              >
+                {data.phone}
+              </TableCell>
+
+              <TableCell
+                align="right"
+                sx={{ borderBottom: "1px solid #F7FAFF" }}
+              >
+                <Box
                   sx={{
-                    borderBottom: "1px solid #F7FAFF",
-                    fontSize: "13.5px",
+                    display: "inline-block",
                   }}
                 >
-                  Sr.
-                </TableCell>
-                <TableCell
-                  sx={{
-                    borderBottom: "1px solid #F7FAFF",
-                    fontSize: "13.5px",
-                  }}
-                >
-                  Name
-                </TableCell>
-
-                <TableCell
-                  sx={{
-                    borderBottom: "1px solid #F7FAFF",
-                    fontSize: "13.5px",
-                  }}
-                >
-                  Email
-                </TableCell>
-
-                <TableCell
-                  sx={{
-                    borderBottom: "1px solid #F7FAFF",
-                    fontSize: "13.5px",
-                  }}
-                >
-                  Phone Number
-                </TableCell>
-
-                <TableCell
-                  align="right"
-                  sx={{
-                    borderBottom: "1px solid #F7FAFF",
-                    fontSize: "13.5px",
-                  }}
-                >
-                  Action
-                </TableCell>
-              </TableRow>
-            </TableHead>
-
-            <TableBody>
-              {(rowsPerPage > 0
-                ? rows.slice(
-                    page * rowsPerPage,
-                    page * rowsPerPage + rowsPerPage
-                  )
-                : rows
-              ).map((row, index) => (
-                <TableRow key={index}>
-                  <TableCell
-                    sx={{
-                      fontWeight: "500",
-                      fontSize: "13px",
-                      borderBottom: "1px solid #F7FAFF",
-                      color: "#260944",
-                      pt: "16px",
-                      pb: "16px",
-                    }}
-                  >
-                    {page * rowsPerPage + index + 1}
-                  </TableCell>
-                  <TableCell
-                    sx={{
-                      fontWeight: "500",
-                      fontSize: "13px",
-                      borderBottom: "1px solid #F7FAFF",
-                      color: "#260944",
-                      pt: "16px",
-                      pb: "16px",
-                    }}
-                  >
-                    {row.name}
-                  </TableCell>
-
-                  <TableCell
-                    sx={{
-                      borderBottom: "1px solid #F7FAFF",
-                      fontSize: "13px",
-                      pt: "16px",
-                      pb: "16px",
-                    }}
-                  >
-                    {row.email}
-                  </TableCell>
-
-                  <TableCell
-                    sx={{
-                      borderBottom: "1px solid #F7FAFF",
-                      fontSize: "13px",
-                      pt: "16px",
-                      pb: "16px",
-                    }}
-                  >
-                    {row.phone}
-                  </TableCell>
-
-                  <TableCell
-                    align="right"
-                    sx={{ borderBottom: "1px solid #F7FAFF" }}
-                  >
-                    <Box
-                      sx={{
-                        display: "inline-block",
-                      }}
+                  <Tooltip title="Remove" placement="top">
+                    <IconButton
+                      aria-label="remove"
+                      size="small"
+                      color="danger"
+                      className="danger"
                     >
-                      <Tooltip title="Remove" placement="top">
-                        <IconButton
-                          aria-label="remove"
-                          size="small"
-                          color="danger"
-                          className="danger"
-                        >
-                          <DeleteIcon fontSize="inherit" />
-                        </IconButton>
-                      </Tooltip>
+                      <DeleteIcon fontSize="inherit" />
+                    </IconButton>
+                  </Tooltip>
 
-                      <Tooltip title="Rename" placement="top">
-                        <IconButton
-                          aria-label="rename"
-                          size="small"
-                          color="primary"
-                          className="primary"
-                        >
-                          <DriveFileRenameOutlineIcon fontSize="inherit" />
-                        </IconButton>
-                      </Tooltip>
-                    </Box>
-                  </TableCell>
-                </TableRow>
-              ))}
-              {rows.length === 0 && (
-                <TableRow>
-                  <TableCell
-                    colSpan={8}
-                    sx={{
-                      borderBottom: "1px solid #F7FAFF",
-                      textAlign: "center",
-                    }}
-                  >
-                    No Record Found
-                  </TableCell>
-                </TableRow>
-              )}
-
-              {emptyRows > 0 && (
-                <TableRow style={{ height: 53 * emptyRows }}>
-                  <TableCell
-                    colSpan={4}
-                    style={{ borderBottom: "1px solid #F7FAFF" }}
-                  />
-                </TableRow>
-              )}
-            </TableBody>
-
-            <TableFooter>
-              <TableRow>
-                <TablePagination
-                  rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
-                  colSpan={8}
-                  count={rows.length}
-                  rowsPerPage={rowsPerPage}
-                  page={page}
-                  SelectProps={{
-                    inputProps: {
-                      "aria-label": "rows per page",
-                    },
-                    native: true,
-                  }}
-                  onPageChange={handleChangePage}
-                  onRowsPerPageChange={handleChangeRowsPerPage}
-                  ActionsComponent={ToDoList}
-                  style={{ borderBottom: "none" }}
-                />
-              </TableRow>
-            </TableFooter>
-          </Table>
-        </TableContainer>
+                  <Tooltip title="Rename" placement="top">
+                    <IconButton
+                      aria-label="rename"
+                      size="small"
+                      color="primary"
+                      className="primary"
+                    >
+                      <DriveFileRenameOutlineIcon fontSize="inherit" />
+                    </IconButton>
+                  </Tooltip>
+                </Box>
+              </TableCell>
+            </>
+          )}
+        />
       </Card>
     </>
   );
