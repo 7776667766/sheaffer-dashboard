04323f61fac72ee9f3727e9156d8ac1464b58e3f
husnain.bixosoft <husnain.bixosoft@gmail.com>
2023-12-14 13:04:06 +0500

    create popup on delete icon

--- components/UIElements/Modal/TransitionsDialog.js
+++ components/UIElements/Modal/TransitionsDialog.js
@@ -1,21 +1,25 @@
-import * as React from 'react';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
-import DialogTitle from '@mui/material/DialogTitle';
-import Slide from '@mui/material/Slide';
-import Card from "@mui/material/Card";
-import { Typography } from "@mui/material";
+import * as React from "react";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import Grid from "@mui/material/Grid";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="up" ref={ref} {...props} />;
 });
 
-export default function TransitionsDialog({modelButton , submitButton, children }) {
+export default function TransitionsDialog({
+  modelButton,
+  submitButtonText,
+  handleSubmit,
+  children,
+  ...props
+}) {
   const [open, setOpen] = React.useState(false);
-
   const handleClickOpen = () => {
     setOpen(true);
   };
@@ -25,36 +29,54 @@ export default function TransitionsDialog({modelButton , submitButton, children 
   };
 
   return (
-    < >
-     
-
+    <>
       <div>
-        {modelButton!= null?  <div onClick={handleClickOpen}>
-          {modelButton}
-        </div>: <Button variant="outlined" onClick={handleClickOpen}>
-          Slide in alert dialog
-        </Button> }
-      
-       
-        
+        {modelButton != null ? (
+          <div onClick={handleClickOpen}>{modelButton}</div>
+        ) : (
+          <Button variant="outlined" onClick={handleClickOpen}>
+            Slide in alert dialog
+          </Button>
+        )}
+
         <Dialog
           open={open}
+          fullWidth={true}
+          maxWidth={"xs"}
           TransitionComponent={Transition}
           keepMounted
           onClose={handleClose}
           aria-describedby="alert-dialog-slide-description"
         >
           <div className="bg-black">
-           
-            <DialogTitle>{""}</DialogTitle>
+            {/* <DialogTitle>{""}</DialogTitle> */}
 
-            <DialogContent>
-              {children}
-            </DialogContent>
-            
-            <DialogActions>
-              {submitButton !== null && submitButton}
-              <Button onClick={handleClose}>Cancel</Button>
+            <DialogContent>{children}</DialogContent>
+
+            <DialogActions sx={{ paddingTop: 0, padding: 1.5 }}>
+              <Grid spacing={1} justifyContent='center' container>
+                {handleSubmit && (
+                  <Grid item xs={6}>
+                     <Button
+                          variant="contained"
+                          fullWidth
+                          onClick={()=>{
+                            handleSubmit();
+                            handleClose();
+                          }}
+                        >
+                          {submitButtonText ?? 'Apply'}
+                        </Button>
+                   
+                    <submitButton onClick={props.handleSubmit} />
+                  </Grid>
+                )}
+                <Grid item xs={6}>
+                  <Button variant="outlined" fullWidth onClick={handleClose}>
+                    Cancel
+                  </Button>
+                </Grid>
+              </Grid>
             </DialogActions>
           </div>
         </Dialog>
