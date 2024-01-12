64a0fb469b0b3834cc1fafb8ee52e782bb2ec07c
Ahmad MUjtaba <ranagee444732@gmail.com>
2024-01-12 14:48:51 +0500

    updating

--- components/Forms/FormLayouts/AddServiceTypeForm.js
+++ components/Forms/FormLayouts/AddServiceTypeForm.js
@@ -55,7 +55,7 @@ const formik = useFormik({
       };
       if (isEditMode) {
         dispatch(
-          editServicesTypeFunApi({
+          editServicesTypeFunApi ({
             data: myServiceData,
             onSuccess: () => {
               console.log("Edit Service Success");
